d0a0f6c3e81a9088159497b72707283175517c6c
赵庆元 <qingyuan@geekniu.com>
2018-10-17 16:17:46 +0800

    test

---
+++ test1/booktest/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import *
+
+
+# Create your views here.
+# render 渲染
+# loader 加载
+# get_template 得到模板
+def index(request):
+    # 获取数据
+    list = BookInfo.objects.all()
+    # 加载模板
+    template = loader.get_template('booktest/index.html')
+    # template
+    ctx = {
+        'list': list
+    }
+    # 返回并渲染模板
+    return HttpResponse(template.render(ctx))
